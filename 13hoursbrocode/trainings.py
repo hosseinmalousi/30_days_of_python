58a9c8c6836809c28ba5ecbd051c7b63d054be45
hosseinM <hossein.malosi@gmail.com>
2026-07-30 14:32:49 +0200

    add trainings.py for basic calculator functionality

---
+++ 13hoursbrocode/trainings.py
@@ -0,0 +1,22 @@
+# #python calc
+
+# operation = input("which operation u wanna use /,*,-,+ :")
+# num1 = float(input("add the first number : "))
+# num2 = float(input("add the second number : "))
+# result = 0
+# if operation == "+" :
+#     result = num1 + num2 
+#     print(round(result,3))
+# elif operation == "-":
+#     result = num1 - num2
+#     print(round(result,3))
+# elif operation == "*" :
+#     result = num1*num2
+#     print(round(result,3))
+# elif operation == "/":
+#     result = num1 + num2
+#     print(round(result,3))
+# else :
+#     print("please insert the right operation")
+
+    
