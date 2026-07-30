b7eb14bfd0e8bf34ae99f4f73b4ab277f28a6673
hosseinM <hossein.malosi@gmail.com>
2026-07-30 14:53:17 +0200

    fix formatting in weight and temperature conversion outputs

--- 13hoursbrocode/trainings.py
+++ 13hoursbrocode/trainings.py
@@ -27,10 +27,10 @@ unit = input("is it in kilogram (K) or pounds (L)").lower()
 
 if unit == "k" :
     unit = "Lb"
-    print(f"{round(weight * 2,20462 , 1)} {unit}")
+    print(f"{round(weight * 2,20462)} {unit}")
 elif unit == "l" :
     unit = "KG"
-    print(f"{round(weight / 2,20462 , 1)} {unit}")
+    print(f"{round(weight / 2,20462)} {unit}")
 else :
     print(f"the unit {unit} is not correct")
     
@@ -41,7 +41,7 @@ unit = input("is it in celsius (C) or fahrenheit (F)").lower()
 
 if unit == "c" :
     unit = "F"
-    print(f"{round((temp * 9) /5 + 32 , 1)} {unit}")
+    print(f"{round((temp * 9) /5 + 32 ,1)} {unit}")
 elif unit == "f" :
     unit = "C"
     print(f"{round((temp - 32) * 5 /9 , 1)} {unit}")
