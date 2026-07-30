19387e4a6113c555fb45788d38b9a40b6ffea23a
hosseinM <hossein.malosi@gmail.com>
2026-07-30 15:21:04 +0200

    refactor: comment out weight and temperature converter sections in trainings.py

--- 13hoursbrocode/trainings.py
+++ 13hoursbrocode/trainings.py
@@ -20,30 +20,40 @@
 #     print("please insert the right operation")
 
     
-## weight converter 
+# ## weight converter 
 
-weight = float(input("how much do u weight ?"))
-unit = input("is it in kilogram (K) or pounds (L)").lower()
+# weight = float(input("how much do u weight ?"))
+# unit = input("is it in kilogram (K) or pounds (L)").lower()
 
-if unit == "k" :
-    unit = "Lb"
-    print(f"{round(weight * 2,20462)} {unit}")
-elif unit == "l" :
-    unit = "KG"
-    print(f"{round(weight / 2,20462)} {unit}")
-else :
-    print(f"the unit {unit} is not correct")
+# if unit == "k" :
+#     unit = "Lb"
+#     print(f"{round(weight * 2,20462)} {unit}")
+# elif unit == "l" :
+#     unit = "KG"
+#     print(f"{round(weight / 2,20462)} {unit}")
+# else :
+#     print(f"the unit {unit} is not correct")
     
-## temp converter 
+# ## temp converter 
 
-temp = float(input("what is the temperture ?"))
-unit = input("is it in celsius (C) or fahrenheit (F)").lower()
+# temp = float(input("what is the temperture ?"))
+# unit = input("is it in celsius (C) or fahrenheit (F)").lower()
 
-if unit == "c" :
-    unit = "F"
-    print(f"{round((temp * 9) /5 + 32 ,1)} {unit}")
-elif unit == "f" :
-    unit = "C"
-    print(f"{round((temp - 32) * 5 /9 , 1)} {unit}")
-else :
-    print(f"the unit {unit} is not correct")
+# if unit == "c" :
+#     unit = "F"
+#     print(f"{round((temp * 9) /5 + 32 ,1)} {unit}")
+# elif unit == "f" :
+#     unit = "C"
+#     print(f"{round((temp - 32) * 5 /9 , 1)} {unit}")
+# else :
+#     print(f"the unit {unit} is not correct")
+
+### validate user input 
+
+# username = input("please enter ur username :\n")
+# print(bool(username.find(" ")))
+
+# if len(username) <= 12 and  username.find(" ") == -1 and username.isalpha() :
+#     print("the username is Valid")
+# else :
+#     print("please enter a user name that :\n 'under 12 characters' \n 'no spaces' \n 'contain no digits'")
